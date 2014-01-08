4a45fdd991a4b9dc04b46a137cdb69f249d151ab
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2014-01-08 10:04:27 +0100

    Updated intel fortran compiler on vsl175

--- system-specifics.sh
+++ system-specifics.sh
@@ -157,7 +157,7 @@ elif [[ $HOSTNAME = lervag-vostro ]]; then
 #{{{1 vsl142 and vsl143
 elif [[ $HOSTNAME = vsl142 || $HOSTNAME = vsl143 ]]; then
   load_compiler_gfortran
-  load_compiler_intel    /usr/local/linux/intel/fc_111
+  load_compiler_intel    /usr/local/linux/intel/fc_2013_sp1
   load_compiler_pgf      /usr/local/pgi 2013
   #load_compiler_sunf90   /usr/local/sun/sunstudio12
   #load_plot_on_runtime   /usr/local/qwt
