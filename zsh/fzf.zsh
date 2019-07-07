69d5815500b0fe5ceac84c12a57b214c26e8bbb5
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2019-07-07 16:00:03 +0200

    Improved fzf CTRL-R

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -8,10 +8,15 @@ fzf-history-widget() {
       -n2..,.. \
       --tiebreak=index \
       --inline-info \
+      --bind 'ctrl-c:accept' \
+      --bind 'ctrl-g:accept' \
+      --bind 'ctrl-q:accept' \
+      --bind    'esc:accept' \
+      --bind 'enter:execute(touch /tmp/ctrlr_mark_1)+accept' \
+      --bind 'ctrl-o:execute(touch /tmp/ctrlr_mark_2)+accept' \
       --exact \
       --query=${(qqq)LBUFFER}" \
       $(__fzfcmd)) )
-  local ret=$?
   if [ -n "$selected" ]; then
     num=$selected[1]
     if [ -n "$num" ]; then
@@ -19,5 +24,10 @@ fzf-history-widget() {
     fi
   fi
   zle reset-prompt
-  zle accept-line
+  if [ -e /tmp/ctrlr_mark_1 ]; then
+    zle accept-line
+  elif [ -e /tmp/ctrlr_mark_2 ]; then
+    zle accept-line-and-down-history
+  fi
+  rm -f /tmp/ctrlr_mark_{1..2} &>/dev/null
 }
