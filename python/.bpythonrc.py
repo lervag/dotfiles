bc761fbdf57fa9a4edb5d229ba66ea93ac658059
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-02-08 14:08:43 +0100

    feat: use ipython instead of bpython

--- python/.bpythonrc.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from sympy import *
-
-x, y, z, t = symbols('x y z t')
-k, m, n = symbols('k m n', integer=True)
-f, g, h = symbols('f g h', cls=Function)
-init_printing()
---
+++ python/.pythonrc.py
@@ -0,0 +1,2 @@
+from rich import inspect, pretty
+pretty.install()
